a56d1c473cae9e3d5dc2056446f95496dd2495c0
Biswa <biswaranjanr@ntucfirstcampus.com>
2020-07-13 18:56:09 +0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './scss/layout.scss';
+
+const App = () => {
+  return (
+    <div className="main-container">
+      <h1>All Accounts</h1>
+    </div>
+  );
+};
+
+export default App;
